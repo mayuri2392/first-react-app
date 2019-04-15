04787f5d20d60636761f07c01f5e3f13de740dcd
Mayuri <mayuripatil2392@gmail.com>
2019-04-15 14:07:30 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+              <h4>Hello World</h4>
           </p>
           <a
             className="App-link"
